1c81f4e094238fd00bcc91978e557815d59ae7e0
pragatibhoyte <pragatibhoyte73@gmail.com>
2026-06-18 16:23:26 +0530

    C programming Assignments on Pattern Printing

---
+++ Assignments/Assignment_26/program02.c
@@ -0,0 +1,55 @@
+/*
+    Accept number of rows and number of columns from user and display below pattern
+    Input : iRow = 4   iCol = 4
+    Output : *   *   *   *
+             *   *   *   #
+             *   *   #   #
+             *   #   #   #
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j= 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid input\n");
+        printf("Enter equal number of rows and columns\n");
+        return;
+    }
+
+    for(i = 1; i<= iRow; i++)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            if(i <= j)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("#\t");
+            }
+        }
+
+        printf("\n");
+    }
+    
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns: ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
